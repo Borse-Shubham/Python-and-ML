6ae4e7e471494581b5e0f52429a01413f243edb7
Borse-Shubham <shubhamborse0911@gmail.com>
2023-09-21 18:52:56 +0530

    Derived Datatypes in C

---
+++ Derived Datatypes/DictionaryDemo.py
@@ -0,0 +1,18 @@
+Batches = {"PPA" : 18500, "Python" : 18700, "LB" : 19000, "Angular" : 19200,"Lsp":18200,"C#" :21000}
+
+print(Batches)
+
+print(type(Batches))
+
+print(len(Batches))
+
+print(Batches["Python"])
+
+for i in Batches:
+    print(i)
+
+for i in Batches:
+    print(Batches[i])
+
+for i in Batches:
+    print(i,Batches[i])
---
+++ Derived Datatypes/ListAsDict.py
@@ -0,0 +1,7 @@
+BatchName = ["ppa","python","Ang","lsp","c#"]
+
+BatchFees = [18500,18700,19200,18200,21000]
+
+for i in range(len(BatchName)):
+    print("Batch name: ", BatchName[i])
+    print("Batch Fees : ", BatchFees[i])
