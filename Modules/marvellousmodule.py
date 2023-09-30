f2d2a75d4f52834afb5338aeb3425a89df620c1c
Borse-Shubham <shubhamborse0911@gmail.com>
2023-10-01 00:01:01 +0530

    Use of Module and their importing

---
+++ Modules/marvellousmodule.py
@@ -0,0 +1,7 @@
+def CheckEven(value):
+    result = value % 2
+
+    if(result == 0):
+        print("number is even")
+    else:
+        print("number is odd")
---
+++ Modules/moduleintro.py
@@ -0,0 +1,18 @@
+
+def Addition(no1,no2):
+    Answer = 0
+    Answer = no1+no2
+    return Answer
+
+
+def Substraction(no1,no2):
+    Answer = 0
+    Answer = no1-no2
+    return Answer
+
+
+
+def Multiplication(no1,no2):
+    Answer = 0
+    Answer = no1*no2
+    return Answer
