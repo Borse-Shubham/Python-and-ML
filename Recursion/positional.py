cdec4bb6ad4d6b8a42783884296bfbdc1c14e152
Borse-Shubham <shubhamborse0911@gmail.com>
2023-10-29 17:33:56 +0530

    Recursion in Python

---
+++ Recursion/positional.py
@@ -0,0 +1,15 @@
+def Display(Name, Age, Marks):
+    print("Name is : ",Name)
+    print("Age is : ",Age)
+    print("Marks are : ", Marks)
+
+def main():
+    print("demonstration of positional arguments")
+
+    Display("Amit",25,89)
+
+    Display("Sagar",21,79)
+
+if __name__=="__main__":
+    main()
+
